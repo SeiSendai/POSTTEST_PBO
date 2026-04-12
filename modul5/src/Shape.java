94c295ca9b07851988332005c511445a6ae6ffd2
SeiSendai <fathir.rahman775@gmail.com>
2026-04-12 20:34:29 +0800

    Create Shape.java

---
+++ modul5/src/Shape.java
@@ -0,0 +1,14 @@
+public class Shape {
+    void calculateArea(int edge){
+        System.out.println(edge*edge)
+    }
+
+    void calculateArea(int length, int width){
+        System.out.println(length*width);
+    }
+
+    void calculateArea(int radius, boolean isCircle){
+        if isCircle
+        ;
+    }
+}
